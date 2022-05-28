d56eacd8a7755e24cdf2b9ad7eab79b7ebbb12e1
Harvey Walker <harveywalker500@gmail.com>
2022-05-28 21:01:57 +0100

    Added saving ship information to file by running target command. Added logging features.

--- calculations.py
+++ calculations.py
@@ -7,13 +7,16 @@ def ship_speed():
     while True:
         # Speed = Distance / Time
         try:
-            distance = float(input("Enter length in meters\n>"))
-            time = float(input("Enter time in seconds\n>"))
+            distance = float(input("Enter length in meters\nS >"))
+            log.debug(f"Distance entered: {distance}")
+            time = float(input("Enter time in seconds\nS >"))
+            log.debug(f"Time entered: {time}")
             speed = float((distance / time) * 1.944)
+            log.debug(f"Calculation done: {distance} / {time} * 1.944 = {speed}")
             print(f"Target speed is {round(speed, 5)} knots.")
             return speed
         except ValueError:
-            log.error("ValueError.")
+            log.error("ValueError with input!")
             print("Enter a number and try again.")
             break
 
@@ -23,23 +26,44 @@ def ship_range():
     while True:
         try:
             # Distance = Height / Centiradian
-            height = float(input("Enter height\n>"))
-            centiradians = float(input("Enter centiradians\n>"))
+            height = float(input("Enter height\nR >"))
+            log.debug(f"Height entered: {height}")
+            centiradians = float(input("Enter centiradians\nR >"))
+            log.debug(f"Centiradians entered: {centiradians}")
             distance = height / centiradians
-            zoom = str(input("Is periscope zoomed? (Y / N)\n>"))
+            log.debug(f"Calculation entered: {height} / {centiradians} = {distance}")
+            zoom = str(input("Is periscope zoomed? (Y / N)\nR >"))
             zoom.lower()
             if zoom == "y":
                 distance = distance * 4
+                log.debug(f"Periscope zoomed.")
             print(f"Target is {round(distance, 5)} hm away.")
             return distance
         except ValueError:
-            log.error("ValueError.")
+            log.error("ValueError with input!")
             print("Enter a number and try again.")
             break
 
 
 def ship_targeting():
-    """Calculates both speed and time."""
+    """Calculates both speed and , and writes to a file."""
     # Combines both distance and speed calculations
-    ship_speed()
-    ship_range()
+    while True:
+        try:
+            target_name = str(input("Enter ship target name\nC >"))
+            log.debug("Target name inputted.")
+        except ValueError:
+            log.error("ValueError with input!")
+            break
+        target_speed = ship_speed()
+        target_range = ship_range()
+        target_info = open("Target Info.txt", "w")
+        log.debug("File opened.")
+        target_info.write(f"""SHIP NAME: {target_name}
+    SHIP SPEED: {target_speed}
+    SHIP RANGE: {target_range}""")
+        log.debug("File written.")
+        target_info.close()
+        log.debug("File closed.")
+        print("Info saved to 'Target Info.txt'")
+        break
