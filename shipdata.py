62587aa65503f5e1cb1cb15bb6e4f6cff815926f
adevries17 <andrew@andrewdevries.net>
2022-06-08 19:49:34 -0500

    add passenger liner data

---
+++ shipdata.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+# passenger liner 1
+pl1 = {
+    length : 165,
+    draught : 7,
+    height : 39
+}
+# passenger liner 2
+pl2 = {
+    length : 170,
+    draught : 8,
+    height : 38
+}
+# passenger liner 3
+pl3 = {
+    length : 150,
+    draught : 7,
+    height : 36
+}
+# passenger liner 4
+pl4 = {
+    length : 162,
+    draught : 7,
+    height : 39
+}
+# passenger liner 5
+pl5 = {
+    length : 140,
+    draught : 6,
+    height : 30
+}
