6e1a82644f5f4984ff5ee17bcc5ae190c61ed68c
adevries17 <andrew@andrewdevries.net>
2022-06-08 21:00:15 -0500

    add ht 1 to 20

--- shipdata.py
+++ shipdata.py
@@ -1,32 +1,31 @@
 #!/usr/bin/env python3
+passenger_liners = {
+    pl1 : { length : 165, draught : 7, height : 39 },
+    pl2 : { length : 170, draught : 8, height : 38 },
+    pl3 : { length : 150, draught : 7, height : 36 },
+    pl4 : { length : 162, draught : 7, height : 39 },
+    pl5 : { length : 140, draught : 6, height : 30 }
+}
 
-# passenger liner 1
-pl1 = {
-    length : 165,
-    draught : 7,
-    height : 39
-}
-# passenger liner 2
-pl2 = {
-    length : 170,
-    draught : 8,
-    height : 38
-}
-# passenger liner 3
-pl3 = {
-    length : 150,
-    draught : 7,
-    height : 36
-}
-# passenger liner 4
-pl4 = {
-    length : 162,
-    draught : 7,
-    height : 39
-}
-# passenger liner 5
-pl5 = {
-    length : 140,
-    draught : 6,
-    height : 30
+heavy_tankers = {
+    ht1 : { length : 142, draught : 5, height : 26 },
+    ht2 : { length : 138, draught : 5, height : 46 },
+    ht3 : { length : 145, draught : 5, height : 49 },
+    ht4 : { length : 144, draught : 6, height : 47 },
+    ht5 : { length : 140, draught : 5, height : 35 },
+    ht6 : { length : 142, draught : 5, height : 24 },
+    ht7 : { length : 150, draught : 6, height : 40 },
+    ht8 : { length : 152, draught : 7, height : 41 },
+    ht9 : { length : 149, draught : 5, height : 37 },
+    ht10 : { length : 153, draught : 6, height : 38 },
+    ht11 : { length : 135, draught : 6, height : 37 },
+    ht12 : { length : 150, draught : 5, height : 28 },
+    ht13 : { length : 145, draught : 5, height : 28 },
+    ht14 : { length : 139, draught : 5, height : 46 },
+    ht15 : { length : 134, draught : 4, height : 43 },
+    ht16 : { length : 137, draught : 5, height : 42 },
+    ht17 : { length : 132, draught : 6, height : 44 },
+    ht18 : { length : 130, draught : 6, height : 41 },
+    ht19 : { length : 129, draught : 6, height : 36 },
+    ht20 : { length : 125, draught : 4, height : 47 }
 }
